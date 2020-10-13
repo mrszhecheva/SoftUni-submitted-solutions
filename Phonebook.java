cec4b7611904fd4c4774b6d354036840d21b32a1
Denitsa Zhecheva <71278440+mrszhecheva@users.noreply.github.com>
2020-10-13 17:57:09 +0300

    =Initial Commit

---
+++ Phonebook.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Phonebook {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> phonebook = new HashMap<>();
+
+        String input = scanner.nextLine();
+        while (!input.equals("search")) {
+            String[] tokens = input.split("-");
+            String name = tokens[0];
+            String phoneNumber = tokens[1];
+            phonebook.put(name, phoneNumber);
+
+            input = scanner.nextLine();
+        }
+
+        input = scanner.nextLine();
+        while (!input.equals("stop")) {
+            if (phonebook.containsKey(input)) {
+                    System.out.printf("%s -> %s%n", input, phonebook.get(input));
+            } else {
+                System.out.printf("Contact %s does not exist.%n", input);
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
